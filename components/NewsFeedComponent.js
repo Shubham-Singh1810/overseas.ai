ce29bdbd4cf05eb8053e14a16aac8d3f16baac6f
Shubham-Singh1810 <hittheshubham1810@gmail.com>
2024-02-23 14:00:52 +0530

    after fixing newsfeed

--- components/NewsFeedComponent.js
+++ components/NewsFeedComponent.js
@@ -1,8 +1,8 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet,Linking, Text, View} from 'react-native';
 import {useState} from 'react';
 import {WebView} from 'react-native-webview';
 import {TouchableOpacity} from 'react-native-gesture-handler';
-import { useGlobalState } from '../GlobalProvider';
+import {useGlobalState} from '../GlobalProvider';
 const NewsFeedComponent = ({value}) => {
   const [showFullArticle, setShowFullArticle] = useState(false);
   const {newTranslation} = useGlobalState();
@@ -10,21 +10,55 @@ const NewsFeedComponent = ({value}) => {
     <>
       <View style={styles.main}>
         <Text style={styles.title}>
-        {value.Title.split(" ")[0][0].toUpperCase()}{value.Title.substring(1)}
+          {value.ArticleTitle.split(' ')[0][0].toUpperCase()}
+          {value.ArticleTitle.substring(1)}
         </Text>
+        {value?.ArticleTitle ==null && <Text style={styles.title}>
+          Overseas News
+          </Text>}
+        
+        <Image
+          source={{
+            uri: value?.image,
+          }}
+          style={{
+            height: 200,
+            width: '100%',
+            borderRadius: 3,
+          }}
+        />
         <Text style={styles.summery}>
-        {value.Summary?.split(" ")[0][0].toUpperCase()}{value.Summary?.substring(1)}
+          {value.summary?.split(' ')[0][0].toUpperCase()}
+          {value.summary?.substring(1)}
         </Text>
         <TouchableOpacity
-          style={{flexDirection: 'row', justifyContent:"flex-end"}}
-          onPress={() => setShowFullArticle(!showFullArticle)}>
+          style={{flexDirection: 'row', justifyContent: 'flex-end'}}
+          onPress={() => Linking.openURL(value.Link)
+            .then(supported => {
+              if (!supported) {
+                console.warn(`Cannot handle URL: ${value.Link}`);
+              }
+            })
+            .catch(err => console.error('An error occurred', err))}>
           <Text style={styles.link}>
-            {showFullArticle ? newTranslation?.hideDetails : newTranslation?.readDetails}
+            {showFullArticle
+              ? newTranslation?.hideDetails
+              : newTranslation?.readDetails}
           </Text>
         </TouchableOpacity>
       </View>
       {showFullArticle && (
-        <View style={{height: 2500, marginHorizontal: 15, paddingBottom:10, backgroundColor:"white", borderTopLeftRadius: 0, borderTopRightRadius: 0, borderBottomLeftRadius: 20, borderBottomRightRadius: 20}}>
+        <View
+          style={{
+            height: 2500,
+            marginHorizontal: 15,
+            paddingBottom: 10,
+            backgroundColor: 'white',
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0,
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+          }}>
           <WebView
             source={{uri: value?.link}}
             javaScriptEnabled={true}
@@ -51,10 +85,12 @@ const NewsFeedComponent = ({value}) => {
             style={{flex: 1}}
           />
           <TouchableOpacity
-            style={{flexDirection: 'row', padding:10}}
+            style={{flexDirection: 'row', padding: 10}}
             onPress={() => setShowFullArticle(!showFullArticle)}>
             <Text style={styles.link}>
-              {showFullArticle ? newTranslation?.hideDetails : newTranslation?.readDetails}
+              {showFullArticle
+                ? newTranslation?.hideDetails
+                : newTranslation?.readDetails}
             </Text>
           </TouchableOpacity>
         </View>
@@ -71,27 +107,26 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     backgroundColor: '#fff',
     elevation: 4,
-    marginVertical:10
+    marginVertical: 10,
   },
   title: {
     color: '#000',
     fontFamily: 'Poppins',
     fontSize: 20,
     fontWeight: '400',
-    textAlign:"justify"
+    textAlign: 'justify',
   },
   summery: {
     marginVertical: 12,
     color: '#00111F',
     fontFamily: 'mono-space',
     fontSize: 15,
-    lineHeight:20,
-    textAlign:"justify"
+    lineHeight: 20,
+    textAlign: 'justify',
   },
   link: {
-   color:"#007bff",
+    color: '#007bff',
     borderColor: '#007bff',
     borderBottomWidth: 0.2,
-    
   },
 });
